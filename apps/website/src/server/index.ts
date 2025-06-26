03d8b11fa442c7e4ef2e81a17e5f72d89f111242
John Marlo Lapiz <91590512+lazuee@users.noreply.github.com>
2025-06-26 11:53:31 +0000

    update
    - removed unnecessary "VERCEL=1" from vercel.json
    - update server to handle vercel

--- apps/website/src/server/index.ts
+++ apps/website/src/server/index.ts
@@ -1,22 +1,28 @@
+import { env } from "node:process";
 import { serve } from "@hono/node-server";
 import { serveStatic } from "@hono/node-server/serve-static";
+import { handle } from "@hono/node-server/vercel";
 import { Hono } from "hono";
+
 import reactRouterRequestHandler from "virtual:react-router/request-handler";
 
+const isVercel = env.VERCEL === "1";
 const app = new Hono();
-app.use(
-  "/*",
-  serveStatic({
-    root: "public/",
-  }),
-);
-app.use(
-  "/client/*",
-  serveStatic({
-    rewriteRequestPath: (path) => path.replace(/^\/client/, ""),
-    root: "./dist/client",
-  }),
-);
+if (!isVercel) {
+  app.use(
+    "/*",
+    serveStatic({
+      root: "public/",
+    }),
+  );
+  app.use(
+    "/client/*",
+    serveStatic({
+      rewriteRequestPath: (path) => path.replace(/^\/client/, ""),
+      root: "./dist/client",
+    }),
+  );
+}
 
 app.get("/.well-known/appspecific/com.chrome.devtools.json", (ctx) => {
   return ctx.notFound();
@@ -26,7 +32,16 @@ app.use("*", (ctx) => {
   return reactRouterRequestHandler(ctx.req.raw);
 });
 
-serve({ ...app, port: 3000 });
-console.log("Server listening on port 3000 (http://localhost:3000)");
+const createServer = () => {
+  if (!isVercel) {
+    serve({ ...app, port: 3000 });
+    console.log("Server listening on port 3000 (http://localhost:3000)");
+  } else {
+    const handler: ReturnType<typeof handle> = (req, res) => {
+      return handle(app)(req, res);
+    };
+    return handler;
+  }
+};
 
-export default app;
+export default createServer();
